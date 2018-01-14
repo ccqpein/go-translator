028fc04a8d57d074fdcca15e5e5be0569a7adb61
ccQpein <ccqpein@protonmail.com>
2018-01-13 21:26:27 -0500

    a bit

--- generator/generator.go
+++ generator/generator.go
@@ -1,3 +1,9 @@
 // using scope table and template to create go source code
 
 package generator
+
+func ReadScope(scopetable map[string][]string, keyword string) []string {
+	return scopetable[keyword]
+}
+
+func ReadPredicate()
