e3c06270f80e856105145736b5efb5d6e69f766a
ccQpein <ccqpein@protonmail.com>
2018-02-08 11:40:39 -0500

    a bit

--- generator/keywords.go
+++ generator/keywords.go
@@ -1,6 +1,13 @@
 package generator
 
-var keywords = map[string]string{
-	"func":   "none",
-	"return": "none",
+import "strings"
+
+var keywords = map[string]func([]string) string{
+	"func": func(a []string) string { return "" },
+	"return": func(a []string) string {
+		result := "return "
+		result += strings.Join(a[1:], ", ")
+
+		return result
+	},
 }
