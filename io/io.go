84eead33b35cc3aaddc099a9a2e0a490c963d73b
ccQpein <ccqpein@protonmail.com>
2018-02-18 19:02:37 -0500

    a bit

--- io/io.go
+++ io/io.go
@@ -1,3 +1,17 @@
 package io
 
-func JointFile() {}
+import "os"
+
+func JointFile(path string, fileFlow chan string) {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for part := range fileFlow {
+		if _, err = f.WriteString(part); err != nil {
+			panic(err)
+		}
+	}
+}
